f1b5558b7e81e32e0f87459fd1d72dadbcc43a0d
clenylda kennedy <salgado.cfk@gmail.com>
2025-04-03 22:30:28 -0300

    Aula de javascript

---
+++ aula-js-01/ola.js
@@ -0,0 +1,16 @@
+console.log("olá mundo");
+
+var msg = "Eu quero um Camaro Amarelo!";
+console.log(msg);
+
+let resposta = "meus alunos são lindos! #CAM";
+console.log(resposta);
+
+const turma = "eu ♥ o meu cachorro";
+console.log(turma);
+
+resposta = "quero o Camaro Amarelo, please?";
+
+
+//turma = "teste";
+//console.log(turma);
